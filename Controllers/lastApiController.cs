23db55a9c83badcfe90ca22b8f4efa3abe974d1f
moath <justsample403@gmail.com>
2025-05-03 21:38:29 +0300

    first commit

---
+++ Controllers/lastApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apis11od.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class lastApiController : ControllerBase
+    {
+        [HttpGet]
+        public string GetMyName()
+        {
+            return "Moath is good";
+        }
+    }
+}
